7c13e213a8d248d081c4f2dd7d14f70295f43c11
melok2207 <132545373+melok2207@users.noreply.github.com>
2024-10-29 18:40:40 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,13 +1,19 @@
-"Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
-"Поверніть дані користувачів у форматі масиву"
-"Дані мають включати такі поля, як id та name."
+const fetch = require('node-fetch'); 
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+async function fetchUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users'); 
+    const users = await response.json(); 
 
-function fetchUsers() {
-  // Ваш код
+
+    return users.map(user => ({ id: user.id, name: user.name }));
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return []; 
+  }
 }
 
-console.log(fetchUsers())
+// Використання функції
+fetchUsers().then(users => console.log(users));
 
 module.exports = fetchUsers;
