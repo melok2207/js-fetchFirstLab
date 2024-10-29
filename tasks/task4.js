bf5822747f7603532ebdf7816c36f23390f0984b
melok2207 <132545373+melok2207@users.noreply.github.com>
2024-10-29 18:45:53 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -1,13 +1,20 @@
-"Ваш код повинен зробити DELETE-запит до вказаного URL, де {userId} – це ID користувача, якого потрібно видалити."
-"Поверніть статус відповіді сервера після видалення."
+const fetch = require('node-fetch'); 
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+async function deleteUser(id) {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'DELETE'
+    });
 
-
-function deleteUser(id) {
-  // Ваш код
+    return response.status; 
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return null; 
+  }
 }
 
-console.log(deleteUser(1));
+
+deleteUser(1)
+  .then(status => console.log('Status:', status));
 
 module.exports = deleteUser;
